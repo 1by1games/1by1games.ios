a8bc1eae282a53be656bf53d42a8c44eb46e3ef6
Maxime Antoine <mantoine1@myges.fr>
2021-03-26 23:38:52 +0100

    creation projet

---
+++ 1by1/1by1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  1by1
+//
+//  Created by Maxime Antoine on 26/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 1by1/1by1/_by1App.swift
@@ -0,0 +1,17 @@
+//
+//  _by1App.swift
+//  1by1
+//
+//  Created by Maxime Antoine on 26/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct _by1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
